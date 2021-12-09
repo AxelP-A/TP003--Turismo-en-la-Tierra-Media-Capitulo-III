b1126f3627109cb3fa42ccfcf6911ca40256dbd2
AxelP-A <axelpablo_93@hotmail.com>
2021-12-08 23:58:08 -0300

    V2.2.0
    Se ordenan las atracciones correctamente, se separan las vistas en
    función a si el usuario es admin o no, empezamos el promocionService.

---
+++ src/main/java/services/PromocionService.java
@@ -0,0 +1,60 @@
+package services;
+
+import java.util.List;
+
+import model.Promocion;
+import persistence.AtraccionDAO;
+import persistence.PromocionDAO;
+import persistence.commons.DAOFactory;
+
+public class PromocionService {
+
+	public List<Promocion> list() {
+		return DAOFactory.getPromocionDAO().findAll();
+	}
+
+	public Promocion create(int id, String nombre, int costo, double tiempo, int cupo, String tipo) {
+
+		Promocion promocion = new Promocion(-1, nombre, costo, tiempo, cupo, tipo);	//acá va el tipo de promo, necesitamos los 3 constructores.
+		
+
+		if (promocion.isValid()) {
+			PromocionDAO promocionDAO = DAOFactory.getPromocionDAO();
+			promocionDAO.insert(promocion);
+			// XXX: si no devuelve "1", es que hubo más errores
+		}
+		return promocion;
+	}
+
+	public Promocion update(int id, String nombre, int costo, double tiempo, int cupo, String tipo) {
+
+		PromocionDAO promocionDAO = DAOFactory.getPromocionDAO();
+		Promocion promocion = promocionDAO.find(id);
+
+		promocion.setNombre(nombre);
+		promocion.setCostoDeVisita(costo);
+		promocion.setTiempoNecesario(tiempo);
+		promocion.setCupo(cupo);
+		promocion.setTipo(tipo);
+
+		if (promocion.isValid()) {
+			promocionDAO.update(promocion);
+			// XXX: si no devuelve "1", es que hubo más errores
+		}
+		return promocion;
+	}
+
+	// Revisar manera de implementar correctamente, ya que nos null no los toma bien.
+	public void delete(int id) {
+		Promocion atraccion = new Atraccion(id, null, null, null, null, null);
+		
+		AtraccionDAO atraccionDAO = DAOFactory.getPromocionDAO();
+		atraccionDAO.delete(atraccion);
+	}
+
+	public Promocion find(int id) {
+		PromocionDAO promocionDAO = DAOFactory.getPromocionDAO();
+		return promocionDAO.findByPromocionId(id);
+	}
+
+}
