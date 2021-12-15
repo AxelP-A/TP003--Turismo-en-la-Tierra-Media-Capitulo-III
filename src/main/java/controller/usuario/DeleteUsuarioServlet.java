877a7b4d3e4a1afed2f3eb563b5f0e580a8a1c6a
AxelP-A <axelpablo_93@hotmail.com>
2021-12-15 14:09:24 -0300

    Diferencias entre index de usuario entre mi proyecto y el subido a gitHub

---
+++ src/main/java/controller/usuario/DeleteUsuarioServlet.java
@@ -0,0 +1,31 @@
+package controller.usuario;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import services.UsuarioService;
+
+@WebServlet("/usuario/delete.do")
+public class DeleteUsuarioServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = -5948538887801392265L;
+	private UsuarioService usuarioService;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		this.usuarioService= new UsuarioService();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer id = Integer.parseInt(req.getParameter("id"));
+
+		usuarioService.delete(id);
+		resp.sendRedirect("/Tp003-TurismoEnLaTierraMedia/usuario/index.do");
+	}
+}
